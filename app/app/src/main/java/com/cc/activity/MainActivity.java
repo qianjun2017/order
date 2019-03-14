6c10003eab1ddf61a770d86bd24702a7a5950bf0
yuanwenshu <yuanwenshu@cq.chinamobile.com>
2019-03-14 20:31:21 +0800

    列表样式、点击事件

--- app/app/src/main/java/com/cc/activity/MainActivity.java
+++ app/app/src/main/java/com/cc/activity/MainActivity.java
@@ -1,14 +1,18 @@
 package com.cc.activity;
 
 import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import com.cc.adapter.AppListAdapter;
 import com.cc.bean.App;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MainActivity extends TopBarBaseActivity {
 
@@ -27,15 +31,50 @@ public class MainActivity extends TopBarBaseActivity {
         appList.clear();
         App alipay = new App();
         alipay.setName("支付宝");
-        alipay.setMonitor(Boolean.FALSE);
+        alipay.setMonitor(Boolean.TRUE);
         appList.add(alipay);
         App wechat = new App();
         wechat.setName("微信");
         wechat.setMonitor(Boolean.FALSE);
         appList.add(wechat);
         listView = findViewById(R.id.app_list);
-        AppListAdapter appListAdapter = new AppListAdapter(MainActivity.this, appList, R.layout.app_list_item);
-        listView.setAdapter(appListAdapter);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, appList.stream().map(app->app.getName()).collect(Collectors.toList()).toArray(new String[0]));
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        for(App app: appList){
+            if(app.getMonitor()){
+                int position = appList.indexOf(app);
+                listView.performItemClick(listView.getChildAt(position), position, listView.getItemIdAtPosition(position));
+            }
+        }
+    }
+
+    public void save(View view){
+        SparseBooleanArray checkedApps = listView.getCheckedItemPositions();
+        for(int i = 0; i < checkedApps.size(); i ++){
+            Boolean checked = checkedApps.valueAt(i);
+            if(checked){
+                appList.get(i).setMonitor(Boolean.TRUE);
+            }
+        }
+        StringBuffer buffer = new StringBuffer("当前监控通知：");
+        for (App app: appList){
+            if(app.getMonitor()){
+                buffer.append(app.getName());
+            }
+        }
+        Toast.makeText(getApplicationContext(), buffer.substring(0), Toast.LENGTH_SHORT).show();
     }
 
 }
