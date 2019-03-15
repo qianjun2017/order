555d904f668d526ab4373ff912bc7d22db6e9bd4
yuanwenshu <yuanwenshu@cq.chinamobile.com>
2019-03-15 16:40:48 +0800

    监听服务获取本地监听配置

--- app/app/src/main/java/com/cc/service/NotificationCollectorService.java
+++ app/app/src/main/java/com/cc/service/NotificationCollectorService.java
@@ -8,7 +8,12 @@ import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 import com.cc.bean.App;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 
 public class NotificationCollectorService extends NotificationListenerService {
@@ -40,7 +45,40 @@ public class NotificationCollectorService extends NotificationListenerService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        appList = fetch();
         Log.v("监听通知服务", "NotificationCollectorService正在onStartCommand");
         return super.onStartCommand(intent, flags, startId);
+    }
+
+    /**
+     * 获取本地存储的app设置
+     * @return
+     */
+    public List<App> fetch(){
+        FileInputStream fis=null;
+        byte[] buffer=null;
+        try {
+            fis=openFileInput("apps");
+            buffer=new  byte[fis.available()];
+            fis.read(buffer);
+        }catch (FileNotFoundException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(fis!=null){
+                try {
+                    fis.close();
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        if(buffer==null){
+            return null;
+        }
+        Gson gson = new Gson();
+        List<App> appList = gson.fromJson(new String(buffer), new TypeToken<List<App>>(){}.getType());
+        return appList;
     }
 }
